d5e6ce93de783e8c31bf14afe50d16551f713328
Alabab95 <benabdallahalaeddine@yahoo.com>
2021-05-02 07:58:18 +0100

    added courses list and author actions and reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,15 @@
-import { applyMiddleware, createStore, compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
-export default function configureStore(initialeState) {
+export default function configureStore(initialState) {
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for redux dev tools
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+
   return createStore(
     rootReducer,
-    initialeState,
-    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant())),
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
