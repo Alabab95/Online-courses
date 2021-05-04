c42748af82946f504f4f6561e5d1630a702f20e0
Alabab95 <benabdallahalaeddine@yahoo.com>
2021-05-04 06:22:27 +0100

    add courses form , textinput , selectinput

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => (
   <div className="jumbotron">
-    <h1>Pluralsight Administration</h1>
-    <p>React, Redux and React Router for ultra-responsive web apps.</p>
+    <h1>Online IT courses</h1>
+    <p>Web developement, Mobile developement , Machine Learning and more </p>
     <Link to="about" className="btn btn-primary btn-lg">
       Learn more
     </Link>
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
-import configureStore from './redux/configureStore';
-import { Provider as ReduxProvider} from 'react-redux';
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
 const store = configureStore();
+
 render(
   <ReduxProvider store={store}>
     <Router>
       <App />
     </Router>
-  </ReduxProvider>
-  ,
+  </ReduxProvider>,
   document.getElementById("app")
 );
